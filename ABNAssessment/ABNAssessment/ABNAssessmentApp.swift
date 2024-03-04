2702d07a5e6b1804042c6555e95b5dc68e0c938e
Jolanda Arends <mail@jolandaarends.com>
2024-03-04 14:11:31 +0100

    Create xcode project with configuration for iPhone and iOS 15+

---
+++ ABNAssessment/ABNAssessment/ABNAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABNAssessmentApp.swift
+//  ABNAssessment
+//
+//  Created by Jolanda Arends on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ABNAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
